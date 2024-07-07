5cfe883796e395fac8e6a1fb0d683fb236365840
sky <67554336+sky-is-winning@users.noreply.github.com>
2024-07-07 19:01:34 +0100

    initial commit

---
+++ start.ps1
@@ -0,0 +1,23 @@
+$scriptDir = $PSScriptRoot
+
+$commands = @(
+    "cd $scriptDir\houdini
+    python bootstrap.py login",
+    "cd $scriptDir\houdini
+    python bootstrap.py world",
+    "cd $scriptDir\dash
+    python bootstrap.py -c config.py",
+    "cd $scriptDir\web
+    npm run start",
+    "cd $scriptDir\snowflake
+    python main.py",
+    "websockify localhost:6113 localhost:6112",
+    "websockify localhost:9876 localhost:9875"
+)
+
+$wtPath = "wt.exe"
+
+foreach ($command in $commands) {
+    Start-Process $wtPath -ArgumentList @("-w", "0", "new-tab", "powershell.exe", "-NoExit", "-Command", $command)
+    Start-Sleep -Milliseconds 300
+}
---
+++ start.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Define the script directory
+scriptDir=$(dirname "$(readlink -f "$0")")
+
+# Define screen session names and commands
+commands=(
+    "cd $scriptDir/houdini && python bootstrap.py login"
+    "cd $scriptDir/houdini && python bootstrap.py world"
+    "cd $scriptDir/dash && python bootstrap.py -c config.py"
+    "cd $scriptDir/web && npm run start"
+    "cd $scriptDir/snowflake && python main.py",
+    "websockify localhost:6113 localhost:6112",
+    "websockify localhost:9876 localhost:9875"
+)
+
+# Function to start a screen session with a unique name
+start_screen_session() {
+    local session_name="$1"
+    local command="$2"
+
+    # Check if a screen session with this name already exists and terminate it
+    screen -S "$session_name" -X quit >/dev/null 2>&1
+
+    # Start a new screen session with the specified name and execute the command
+    screen -dmS "$session_name" bash -c "$command"
+}
+
+# Loop through each command and start a screen session
+for ((i=0; i<${#commands[@]}; i++)); do
+    session_name="penguinscreen_$i"
+    command="${commands[$i]}"
+    start_screen_session "$session_name" "$command"
+
+    # Sleep for 0.3 seconds between starting each screen session
+    sleep 0.3
+done
