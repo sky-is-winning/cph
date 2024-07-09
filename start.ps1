3dc844a076b24f1715a6cca9aa50f9060b94f524
sky <67554336+sky-is-winning@users.noreply.github.com>
2024-07-09 21:24:09 +0100

    run local when on windows

--- start.ps1
+++ start.ps1
@@ -8,7 +8,7 @@ $commands = @(
     "cd $scriptDir\dash
     python bootstrap.py -c config.py",
     "cd $scriptDir\web
-    npm run start",
+    npm run start 80 local",
     "cd $scriptDir\snowflake
     python main.py",
     "websockify localhost:6113 localhost:6112",
